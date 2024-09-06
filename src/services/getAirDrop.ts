fe75fe4a4a491dfb96ee2133734e7f679641ba88
Buğra Okumuş <bugra.okumus@d-teknoloji.com.tr>
2024-09-06 13:03:24 +0300

    wallet integration

---
+++ src/services/getAirDrop.ts
@@ -0,0 +1,26 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+const { connection } = useConnection();
+const { publicKey } = useWallet();
+
+const getAirdropOnClick = async () => {
+    try {
+        if (!publicKey) {
+            throw new Error("Wallet is not Connected");
+        }
+        const [latestBlockhash, signature] = await Promise.all([
+            connection.getLatestBlockhash(),
+            connection.requestAirdrop(publicKey, 1 * LAMPORTS_PER_SOL),
+        ]);
+        const sigResult = await connection.confirmTransaction(
+            { signature, ...latestBlockhash },
+            "confirmed",
+        );
+        if (sigResult) {
+            alert("Airdrop was confirmed!");
+        }
+    } catch (err) {
+        alert("You are Rate limited for Airdrop");
+    }
+};
