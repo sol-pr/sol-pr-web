131a7f4852937b7f3aa12b0aba7796a0b47d8296
Buğra Okumuş <saltuk.bugraokumus@gmail.com>
2024-09-07 20:14:06 +0300

    rawBody

--- src/app/api/github/route.ts
+++ src/app/api/github/route.ts
@@ -14,25 +14,7 @@ export async function POST(req: Request) {
         return NextResponse.json({ message: 'Invalid request body' }, { status: 400 });
     }
 
-    // Uint8Array'i JSON string'ine dönüştürüyoruz
-    const textDecoder = new TextDecoder('utf-8');
-    const jsonString = textDecoder.decode(rawBody.value);
-
-    // JSON string'ini JavaScript nesnesine dönüştürüyoruz
-    const payload = JSON.parse(jsonString);
-
-    // Payload'dan gerekli bilgileri alıyoruz
-    const webhookPayload = {
-        pusherName: payload.pusher?.name,
-        repositoryUrl: payload.repository?.html_url,
-        branch: payload.ref?.split('/').pop(),  // refs/heads/main gibi bir değerden sadece branch ismini alıyoruz
-        commitMessage: payload.head_commit?.message,
-        commitUrl: payload.head_commit?.url
-    };
-
-    // Konsola yazdırıyoruz
-    console.log('Webhook Payload:', webhookPayload);
 
     // Başarılı yanıt dönüyoruz
-    return NextResponse.json({ message: 'Webhook processed', payload: webhookPayload }, { status: 200 });
+    return NextResponse.json({ message: 'Webhook processed', payload: rawBody }, { status: 200 });
 }
