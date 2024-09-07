e59996a0065da16a9dab4db0e2ba699eefcadc8e
Buğra Okumuş <saltuk.bugraokumus@gmail.com>
2024-09-07 15:12:20 +0300

    3d homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 "use client";
 
+import { useWallet } from "@solana/wallet-adapter-react";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import Icon3D from "@/components/Icon3D";
+
 export default function Home() {
+  const { publicKey } = useWallet();
+
+  useEffect(() => {
+    if (publicKey) {
+      // publicKey değerini cookie olarak sakla
+      Cookies.set("auth-token", publicKey.toString(), { expires: 7 });
+    } else {
+      // Eğer publicKey yoksa cookie'yi sil
+      Cookies.remove("auth-token");
+    }
+  }, [publicKey]);
+
   return (
     <main className="flex items-center justify-center h-screen">
-      <div className="flex items-center justify-between w-3/4">
+      <div className="flex flex-col sm:flex-row items-center justify-between w-4/5">
         <div className="flex flex-col gap-5">
           <h1 className="md:text-9xl font-bold">
             WELCOME <br />
@@ -11,7 +28,7 @@ export default function Home() {
           </h1>
           <h3 className="text-5xl font-mono">sol-pr</h3>
         </div>
-        <div className="">big image</div>
+        <Icon3D />
       </div>
     </main>
   );
---
+++ src/components/Icon3D.tsx
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-1/2">
+      <Spline scene="https://prod.spline.design/dynilz9H6U7JARIZ/scene.splinecode" />
+    </div>
+  );
+}
