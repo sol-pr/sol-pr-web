245a5047fadef29912014b1a98ac9ce8229af581
Buğra Okumuş <saltuk.bugraokumus@gmail.com>
2024-09-30 16:55:54 +0300

    fix css for mobile

--- src/components/CreateRepo.tsx
+++ src/components/CreateRepo.tsx
@@ -4,7 +4,7 @@ import FlowStepForm from "./FlowStepForm";
 
 const CreateRepo = () => {
   return (
-    <div className="flex-col self-start justify-center pt-14 w-fit">
+    <div className="flex-col self-start justify-center sm:pt-14 w-fit  mx-4 sm:mx-0">
       <Title
         title={"Create Bounty!"}
         description={"You can create boundy for submited pull-requests!"}
